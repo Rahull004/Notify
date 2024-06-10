4a92670f78f2719abe792a35f6d61eb6f44aafce
Rahul Bansal <rahulbansal24072004@gmail.com>
2024-06-10 16:15:11 +0530

    Add colors and font in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,40 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'], 
+      },
+      colors: {
+        gray900: {
+          DEFAULT: "#212121",
+          87: "rgba(33, 33, 33, 0.87",
+          60: "rgba(33, 33, 33, 0.60)",
+          36: "rgba(33, 33, 33, 0.36)",
+        },
+        gray200: "#EEEEEE",
+        black: {
+          DEFAULT: "#000000",
+          32: "rgba(0, 0, 0, 0.32)",
+          24: "rgba(0, 0, 0, 0.24)",
+          12: "rgba(0, 0, 0, 0.12)",
+        },
+        white: {
+          DEFAULT: "#FFFFFF",
+          24: "rgba(255, 255, 255, 0.24)",
+        },
+        blue400: "#42A5F5",
+        blue500: "#2196F3",
+        orange900: "#E65100",
+        orange200: "#FFCC80",
+        green900: "#1B5E20",
+        green200: "#A5D6A7",
+        deepPurple900: "#4527A0",
+        deepPurple200: "#2196F3",
+        red400: "#EF5350",
+        red500: "#F44336",
+      }
+    }
   },
   plugins: [],
 }
