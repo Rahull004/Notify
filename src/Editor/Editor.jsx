23f7fddbe5bc7483187639c31cf5152b3257db2c
Rahul Bansal <rahulbansal24072004@gmail.com>
2024-07-07 19:42:09 +0530

    chore: Update ChatbotPopup and TiptapContext

--- src/Editor/Editor.jsx
+++ src/Editor/Editor.jsx
@@ -12,11 +12,12 @@ import {
 } from "../components/ui/menubar";
 import { TiptapContext } from "../contexts/tiptap_context";
 import { getDraft } from "../appwrite/api.js";
+import { StepContent } from "@mui/material";
 
 const TableMenu = ({ editor }) => [
   {
     id: 1,
-    name: 'Insert Table',
+    name: "Insert Table",
     action: () =>
       editor
         .chain()
@@ -26,290 +27,290 @@ const TableMenu = ({ editor }) => [
   },
   {
     id: 2,
-    name: 'Add Column Before',
+    name: "Add Column Before",
     action: () => editor.chain().focus().addColumnBefore().run(),
   },
   {
     id: 3,
-    name: 'Add Column After',
+    name: "Add Column After",
     action: () => editor.chain().focus().addColumnAfter().run(),
   },
   {
     id: 4,
-    name: 'Delete Column',
+    name: "Delete Column",
     action: () => editor.chain().focus().deleteColumn().run(),
   },
   {
     id: 5,
-    name: 'Add Row Before',
+    name: "Add Row Before",
     action: () => editor.chain().focus().addRowBefore().run(),
   },
   {
     id: 6,
-    name: 'Add Row After',
+    name: "Add Row After",
     action: () => editor.chain().focus().addRowAfter().run(),
   },
   {
     id: 7,
-    name: 'Delete Row',
+    name: "Delete Row",
     action: () => editor.chain().focus().deleteRow().run(),
   },
   {
     id: 8,
-    name: 'Delete Table',
+    name: "Delete Table",
     action: () => editor.chain().focus().deleteTable().run(),
   },
   {
     id: 9,
-    name: 'Merge Cells',
+    name: "Merge Cells",
     action: () => editor.chain().focus().mergeCells().run(),
   },
   {
     id: 11,
-    name: 'Toggle Header Column',
+    name: "Toggle Header Column",
     action: () => editor.chain().focus().toggleHeaderColumn().run(),
   },
   {
     id: 12,
-    name: 'Toggle Header Row',
+    name: "Toggle Header Row",
     action: () => editor.chain().focus().toggleHeaderRow().run(),
   },
   {
     id: 13,
-    name: 'Toggle Header Cell',
+    name: "Toggle Header Cell",
     action: () => editor.chain().focus().toggleHeaderCell().run(),
   },
   {
     id: 14,
-    name: 'Merge Or Split',
+    name: "Merge Or Split",
     action: () => editor.chain().focus().mergeOrSplit().run(),
   },
   {
     id: 15,
-    name: 'Set Cell Attribute',
-    action: () => editor.chain().focus().setCellAttribute('colspan', 2).run(),
+    name: "Set Cell Attribute",
+    action: () => editor.chain().focus().setCellAttribute("colspan", 2).run(),
   },
 ];
 const MenuBarIcon = ({ editor }) => [
   {
     id: 1,
-    name: 'bold',
+    name: "bold",
     icon: Icons.bold,
     onClick: () => editor.chain().focus().toggleBold().run(),
     disable: !editor.can().chain().focus().toggleBold().run(),
-    isActive: editor.isActive('bold') ? 'is-active text-green-700' : '',
+    isActive: editor.isActive("bold") ? "is-active text-green-700" : "",
     hover: false,
     split: false,
   },
   {
     id: 2,
-    name: 'italic',
+    name: "italic",
     icon: Icons.italic,
     onClick: () => editor.chain().focus().toggleItalic().run(),
     disable: !editor.can().chain().focus().toggleItalic().run(),
-    isActive: editor.isActive('italic') ? 'is-active text-green-700' : '',
+    isActive: editor.isActive("italic") ? "is-active text-green-700" : "",
     hover: false,
     split: false,
   },
   {
     id: 21,
-    name: 'underline',
+    name: "underline",
     icon: Icons.underline,
     onClick: () => editor.chain().focus().toggleUnderline().run(),
     disable: false,
-    isActive: editor.isActive('underline') ? 'is-active text-green-700' : '',
+    isActive: editor.isActive("underline") ? "is-active text-green-700" : "",
     hover: false,
     split: false,
   },
   {
     id: 3,
-    name: 'strike',
+    name: "strike",
     icon: Icons.strikethrough,
     onClick: () => editor.chain().focus().toggleStrike().run(),
     disable: !editor.can().chain().focus().toggleStrike().run(),
-    isActive: editor.isActive('strike') ? 'is-active text-green-700' : '',
+    isActive: editor.isActive("strike") ? "is-active text-green-700" : "",
     hover: false,
     split: false,
   },
   {
     id: 4,
-    name: 'code',
+    name: "code",
     icon: Icons.code,
     onClick: () => editor.chain().focus().toggleCode().run(),
     disable: !editor.can().chain().focus().toggleCode().run(),
-    isActive: editor.isActive('code') ? 'is-active text-green-700' : '',
+    isActive: editor.isActive("code") ? "is-active text-green-700" : "",
     hover: false,
     split: true,
   },
   {
     id: 5,
-    name: 'heading1',
+    name: "heading1",
     icon: Icons.h1,
     onClick: () => editor.chain().focus().toggleHeading({ level: 1 }).run(),
     disable: false,
-    isActive: editor.isActive('heading', { level: 1 })
-      ? 'is-active text-green-700'
-      : '',
+    isActive: editor.isActive("heading", { level: 1 })
+      ? "is-active text-green-700"
+      : "",
     hover: false,
     split: false,
   },
   {
     id: 6,
-    name: 'heading2',
+    name: "heading2",
     icon: Icons.h2,
     onClick: () => editor.chain().focus().toggleHeading({ level: 2 }).run(),
     disable: false,
-    isActive: editor.isActive('heading', { level: 2 })
-      ? 'is-active text-green-700'
-      : '',
+    isActive: editor.isActive("heading", { level: 2 })
+      ? "is-active text-green-700"
+      : "",
     hover: false,
     split: false,
   },
   {
     id: 13,
-    name: 'heading3',
+    name: "heading3",
     icon: Icons.h3,
     onClick: () => editor.chain().focus().toggleHeading({ level: 3 }).run(),
     disable: false,
-    isActive: editor.isActive('heading', { level: 3 })
-      ? 'is-active text-green-700'
-      : '',
+    isActive: editor.isActive("heading", { level: 3 })
+      ? "is-active text-green-700"
+      : "",
     hover: false,
     split: false,
   },
   {
     id: 14,
-    name: 'heading4',
+    name: "heading4",
     icon: Icons.h4,
     onClick: () => editor.chain().focus().toggleHeading({ level: 4 }).run(),
     disable: false,
-    isActive: editor.isActive('heading', { level: 4 })
-      ? 'is-active text-green-700'
-      : '',
+    isActive: editor.isActive("heading", { level: 4 })
+      ? "is-active text-green-700"
+      : "",
     hover: false,
     split: false,
   },
   {
     id: 15,
-    name: 'heading5',
+    name: "heading5",
     icon: Icons.h5,
     onClick: () => editor.chain().focus().toggleHeading({ level: 5 }).run(),
     disable: false,
-    isActive: editor.isActive('heading', { level: 5 })
-      ? 'is-active text-green-700'
-      : '',
+    isActive: editor.isActive("heading", { level: 5 })
+      ? "is-active text-green-700"
+      : "",
     hover: false,
     split: false,
   },
   {
     id: 7,
-    name: 'paragraph',
+    name: "paragraph",
     icon: Icons.paragraph,
     onClick: () => editor.chain().focus().setParagraph().run(),
     disable: false,
-    isActive: editor.isActive('paragraph') ? 'is-active text-green-700' : '',
+    isActive: editor.isActive("paragraph") ? "is-active text-green-700" : "",
     hover: false,
     split: true,
   },
   {
     id: 8,
-    name: 'bullet list',
+    name: "bullet list",
     icon: Icons.ul,
     onClick: () => editor.chain().focus().toggleBulletList().run(),
     disable: false,
-    isActive: editor.isActive('bulletList')
-      ? 'is-active text-green-700 list-disc'
-      : '',
+    isActive: editor.isActive("bulletList")
+      ? "is-active text-green-700 list-disc"
+      : "",
     hover: false,
     split: false,
   },
   {
     id: 9,
-    name: 'ordered list',
+    name: "ordered list",
     icon: Icons.ol,
     onClick: () => editor.chain().focus().toggleOrderedList().run(),
     disable: false,
-    isActive: editor.isActive('orderedList')
-      ? 'is-active text-green-700 list-decimal'
-      : '',
+    isActive: editor.isActive("orderedList")
+      ? "is-active text-green-700 list-decimal"
+      : "",
     hover: false,
     split: false,
   },
   {
     id: 16,
-    name: 'align left',
+    name: "align left",
     icon: Icons.alignLeft,
-    onClick: () => editor.chain().focus().setTextAlign('left').run(),
+    onClick: () => editor.chain().focus().setTextAlign("left").run(),
     disable: false,
-    isActive: editor.isActive({ textAlign: 'left' }) ? 'is-active' : '',
+    isActive: editor.isActive({ textAlign: "left" }) ? "is-active" : "",
     hover: false,
     split: false,
   },
   {
     id: 17,
-    name: 'align center',
+    name: "align center",
     icon: Icons.alignCenter,
-    onClick: () => editor.chain().focus().setTextAlign('center').run(),
+    onClick: () => editor.chain().focus().setTextAlign("center").run(),
     disable: false,
-    isActive: editor.isActive({ textAlign: 'center' })
-      ? 'is-active text-green-700 text-center'
-      : '',
+    isActive: editor.isActive({ textAlign: "center" })
+      ? "is-active text-green-700 text-center"
+      : "",
     hover: false,
     split: false,
   },
   {
     id: 18,
-    name: 'align right',
+    name: "align right",
     icon: Icons.alignRight,
-    onClick: () => editor.chain().focus().setTextAlign('right').run(),
+    onClick: () => editor.chain().focus().setTextAlign("right").run(),
     disable: false,
-    isActive: editor.isActive({ textAlign: 'right' }) ? 'is-active' : '',
+    isActive: editor.isActive({ textAlign: "right" }) ? "is-active" : "",
     hover: false,
     split: false,
   },
   {
     id: 19,
-    name: 'align justify',
+    name: "align justify",
     icon: Icons.alignJustify,
-    onClick: () => editor.chain().focus().setTextAlign('justify').run(),
+    onClick: () => editor.chain().focus().setTextAlign("justify").run(),
     disable: false,
-    isActive: editor.isActive({ textAlign: 'justify' }) ? 'is-active' : '',
+    isActive: editor.isActive({ textAlign: "justify" }) ? "is-active" : "",
     hover: false,
     split: true,
   },
   {
     id: 20,
-    name: 'highlight',
+    name: "highlight",
     icon: Icons.bg,
     onClick: () => editor.chain().focus().toggleHighlight().run(),
     disable: false,
-    isActive: editor.isActive('highlight') ? 'is-active text-green-700' : '',
+    isActive: editor.isActive("highlight") ? "is-active text-green-700" : "",
     hover: false,
     split: false,
   },
   {
     id: 10,
-    name: 'code block',
+    name: "code block",
     icon: Icons.codeblock,
     onClick: () => editor.chain().focus().toggleCodeBlock().run(),
     disable: false,
-    isActive: editor.isActive('codeBlock') ? 'is-active text-green-700' : '',
+    isActive: editor.isActive("codeBlock") ? "is-active text-green-700" : "",
     hover: false,
     split: false,
   },
   {
     id: 11,
-    name: 'blockquote',
+    name: "blockquote",
     icon: Icons.blockquote,
     onClick: () => editor.chain().focus().toggleBlockquote().run(),
     disable: false,
-    isActive: editor.isActive('blockquote') ? 'is-active text-green-700' : '',
+    isActive: editor.isActive("blockquote") ? "is-active text-green-700" : "",
     hover: false,
     split: false,
   },
   {
     id: 12,
-    name: 'table',
+    name: "table",
     icon: Icons.table,
     onClick: () =>
       editor
@@ -318,34 +319,33 @@ const MenuBarIcon = ({ editor }) => [
         .insertTable({ rows: 3, cols: 3, withHeaderRow: true })
         .run(),
     disable: false,
-    isActive: editor.isActive('table') ? 'is-active text-green-700' : '',
+    isActive: editor.isActive("table") ? "is-active text-green-700" : "",
     hover: true,
     split: true,
   },
   {
     id: 30,
-    name: 'undo',
+    name: "undo",
     icon: Icons.undo,
     onClick: () => editor.chain().focus().undo().run(),
     disable: !editor.can().undo(),
-    isActive: editor.isActive('table') ? 'is-active text-green-700' : '',
+    isActive: editor.isActive("table") ? "is-active text-green-700" : "",
     hover: false,
     split: false,
   },
   {
     id: 31,
-    name: 'redo',
+    name: "redo",
     icon: Icons.redo,
     onClick: () => editor.chain().focus().redo().run(),
     disable: !editor.can().redo(),
-    isActive: editor.isActive('table') ? 'is-active text-green-700' : '',
+    isActive: editor.isActive("table") ? "is-active text-green-700" : "",
     hover: false,
     split: true,
   },
 ];
 
 function MenuBar({ setImageURL }) {
-  
   const { editor } = useContext(TiptapContext);
   const fileInputRef = useRef(null);
 
@@ -401,7 +401,11 @@ function MenuBar({ setImageURL }) {
               )}
               <MenubarContent>
                 {TableMenu({ editor }).map((menuItem) => (
-                  <MenubarItem key={menuItem.id} onClick={menuItem.action} className=" hover:bg-gray200">
+                  <MenubarItem
+                    key={menuItem.id}
+                    onClick={menuItem.action}
+                    className=" hover:bg-gray200"
+                  >
                     {menuItem.name}
                   </MenubarItem>
                 ))}
@@ -441,8 +445,8 @@ function MenuBar({ setImageURL }) {
   );
 }
 
-function Editor({user,id}) {
-  const { editor, content, editorText } = useContext(TiptapContext);
+function Editor({ id }) {
+  const { editor } = useContext(TiptapContext);
   const [imageURL, setImageURL] = useState(null);
 
   useEffect(() => {
@@ -454,32 +458,15 @@ function Editor({user,id}) {
   }, [imageURL, editor]);
 
   useEffect(() => {
-    if (editor && editorText) {
-      editor
-        .chain()
-        .focus()
-        .insertContent(editorText)
-        .insertContent("<br />")
-        .run();
-    }
-  }, [editorText, editor]);
-
-  useEffect(() => {
-    if (editor && content) {
-      editor.chain().focus().insertContent(content).run();
-    }
-  }, [content, editor]);
-
-  useEffect(()=> {
-     const fetchDraft = async()=> {
+    const fetchDraft = async () => {
       const draft = await getDraft(id);
-      if(draft) {
+      if (draft) {
         editor.chain().focus().insertContent(draft.content).run();
       }
       console.log(draft);
-     }
-     fetchDraft();
-  },[])
+    };
+    fetchDraft();
+  }, []);
   return (
     <div>
       <MenuBar editor={editor} setImageURL={setImageURL} />
