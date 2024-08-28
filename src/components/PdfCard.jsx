03fcb8cdad060667e94e869046902249c89898b3
Vivek Hipparkar <vivekhipparkar@gmail.com>
2024-08-28 13:28:00 +0530

    shadcn add

--- src/components/PdfCard.jsx
+++ src/components/PdfCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
 import { pdfUpload } from "../appwrite/api";
 import { Link } from "react-router-dom";
@@ -9,7 +9,9 @@ export const PdfCard = ({ note, setShowPdfCard }) => {
   const navigate = useNavigate();
   const [file, setFile] = useState(null);
 
-  console.log("pdfs are", pdfs);
+  useEffect(() => {
+    console.log("Initial PDFs:", pdfs);
+  }, []);
 
   const handlePdfClick = (id) => {
     navigate(`/pdfviewer/${id}`);
@@ -22,17 +24,27 @@ export const PdfCard = ({ note, setShowPdfCard }) => {
     if (file) {
       if (file.type === "application/pdf" && file.size <= 5 * 1024 * 1024) {
         setFile(file);
+        console.log("File name:", file.name);
 
         const upload = await pdfUpload({ file, noteId });
         if (upload) {
-          setPdfs([...pdfs, upload]);
+          const updatedPdfs = [
+            ...pdfs,
+            {
+              ...upload,
+              fileName: file.name, // Add file name to uploaded PDF info
+            },
+          ];
+          setPdfs(updatedPdfs);
+          console.log("Updated PDFs array:", updatedPdfs);
         }
-        console.log("upload", upload);
+        console.log("Upload result:", upload);
       } else {
         alert("Please select a PDF file of size less than or equal to 5MB.");
       }
     }
   };
+
   // Step 1: Define state for dragging
   const [isDragging, setIsDragging] = useState(false);
 
@@ -56,8 +68,6 @@ export const PdfCard = ({ note, setShowPdfCard }) => {
     setIsDragging(false); // Reset dragging state
     const files = e.dataTransfer.files;
     if (files.length) {
-      // Assuming handleFileChange can process multiple files
-      // If not, you'll need to adjust this to handle your specific logic
       handleFileChange({ target: { files } });
     }
   };
@@ -67,73 +77,60 @@ export const PdfCard = ({ note, setShowPdfCard }) => {
   };
 
   return (
-    <div className="flex">
-      <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 h-screen">
-        <div className="bg-white py-4 rounded-xl shadow-xl w-3/4 md:w-3/4 h-5/6 overflow-hidden px-16 pt-10">
-          <div className="mb-3 mt-2 pb-6 text-xl font-semibold">
-            <label>
-              Upload Files
-              <input
-                id="pdf-upload"
-                type="file"
-                className="hidden"
-                onChange={handleFileChange}
-              />
-            </label>
-          </div>
-          <div className="h-[87%] w-full flex gap-3">
-            <div
-              className="text-center w-[60%] border-dashed border-2 border-orange-300	0 p-10 bg-amber-100	rounded-xl h-5/6 flex flex-col items-center justify-center"
-              onDragOver={handleDragOver}
-              onDragEnter={handleDragEnter}
-              onDragLeave={handleDragLeave}
-              onClick={handleClick}
-              onDrop={handleDrop}
-            >
-              <img src={Upload} alt="upload" className="w-15 h-15 mx-auto " />
-              <br />
-              <label className="text-lg font-semibold">
-                Drag & Drop your files here
-                <br />
-                or
-                <br />
-                Browse to upload files
-                <input
-                  id="pdf-upload"
-                  type="file"
-                  className="hidden"
-                  onChange={handleFileChange}
-                />
-              </label>
-              <br />
-              <br />
-              <div className="h-fit items-center text-sm justify-end">
-                Only PDF files of max size of 5MB
-              </div>
-            </div>
-
-            <div className=" overflow-auto flex justify-center items-center h-12">
-              {pdfs.map((pdf, index) => (
-                <Link
-                  to={`/pdfviewer/${pdf.$id}`}
-                  key={pdf.$id}
-                  onClick={() => handlePdfClick(pdf.$id)}
-                  className="cursor-pointer max-w-20 mx-2 rounded-lg p-2 bg-red-600"
-                >
-                  <p className="text-lg text-white">PDF {index + 1}</p>
-                </Link>
-              ))}
-            </div>
-          </div>
-          <div className="relative">
-            <button
-              className="absolute bottom-0 right-0 bg-red-500 hover:bg-red-700 text-white font-bold py-3 px-5 rounded"
-              onClick={() => setShowPdfCard(false)}
-            >
-              Close
-            </button>
+    <div className="flex items-center justify-center fixed inset-0 bg-black bg-opacity-50 z-50 h-screen">
+      <div className="bg-white rounded-xl shadow-xl w-3/4 md:w-3/4 h-5/6 overflow-hidden flex p-14">
+        {/* Upload Area */}
+        <div
+          className="text-center border-dashed border-2 border-orange-300 bg-amber-100 rounded-xl w-3/5 flex flex-col items-center justify-center"
+          onDragOver={handleDragOver}
+          onDragEnter={handleDragEnter}
+          onDragLeave={handleDragLeave}
+          onClick={handleClick}
+          onDrop={handleDrop}
+        >
+          <img src={Upload} alt="upload" className="w-15 h-15 mx-auto" />
+          <br />
+          <label className="text-lg font-semibold">
+            Drag & Drop your files here
+            <br />
+            or
+            <br />
+            Browse to upload files
+            <input
+              id="pdf-upload"
+              type="file"
+              className="hidden"
+              onChange={handleFileChange}
+            />
+          </label>
+          <br />
+          <br />
+          <div className="h-fit items-center text-sm justify-end">
+            Only PDF files of max size of 5MB
           </div>
         </div>
+
+        {/* PDF Grid */}
+        <div className="w-2/3 h-full overflow-auto px-10">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {pdfs.map((pdf) => (
+              <Link
+                to={`/pdfviewer/${pdf.$id}`}
+                key={pdf.$id}
+                onClick={() => handlePdfClick(pdf.$id)}
+                className="cursor-pointer rounded-lg p-3 bg-red-600 flex items-center justify-center"
+              >
+                <p className="text-lg text-white truncate">{pdf.fileName}</p>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="absolute bottom-0 right-0 bg-red-500 hover:bg-red-700 text-white font-bold py-3 px-5 rounded">
+        <button onClick={() => setShowPdfCard(false)}>
+          Close
+        </button>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,93 +1,77 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
+  darkMode: ["class"],
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
   ],
+  prefix: "",
   theme: {
-    extend: {
-      fontFamily: {
-        'rob': ['Roboto', 'sans-serif'], 
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
       },
+    },
+    extend: {
       colors: {
-        foreground: 'hsl(var(--foreground))',
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))',
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
         },
         destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))',
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
         },
         muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))',
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
         },
         accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))',
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
         },
         popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))',
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
         },
         card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
         },
-        gray900: {
-          DEFAULT: "#212121",
-          87: "rgba(33, 33, 33, 0.87)",
-          60: "rgba(33, 33, 33, 0.60)",
-          36: "rgba(33, 33, 33, 0.36)",
-        },
-        gray200: "#EEEEEE",
-        black: {
-          DEFAULT: "#000000",
-          32: "rgba(0, 0, 0, 0.32)",
-          24: "rgba(0, 0, 0, 0.24)",
-          12: "rgba(0, 0, 0, 0.12)",
-        },
-        white: {
-          DEFAULT: "#FFFFFF",
-          24: "rgba(255, 255, 255, 0.24)",
-        },
-        blue400: "#42A5F5",
-        blue500: "#2196F3",
-        blue600: "#1E88E5",
-        orange900: "#E65100",
-        orange200: "#FFCC80",
-        green900: "#1B5E20",
-        green200: "#A5D6A7",
-        deepPurple900: "#4527A0",
-        deepPurple200: "#2196F3",
-        red400: "#EF5350",
-        red500: "#F44336",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
-        'accordion-down': {
-          from: { height: 0 },
-          to: { height: 'var(--radix-accordion-content-height)' },
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
         },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: 0 },
-        },
-        popup: {
-          from: { transform: 'scale(0.8)', opacity: '0' },
-          to: { transform: 'scale(1)', opacity: '1' },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
         },
       },
       animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
-        'popup': 'popup 0.1s ease-out',
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
-    }
+    },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require("tailwindcss-animate")],
 }
