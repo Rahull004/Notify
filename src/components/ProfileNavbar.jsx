6d1c881e068e5902ee4a73ff4b462e0600cc8bb2
Harsh Rai <htc38199@gmail.com>
2024-08-29 05:25:57 +0530

    fixed navbar ui

--- src/components/ProfileNavbar.jsx
+++ src/components/ProfileNavbar.jsx
@@ -28,14 +28,19 @@ function ProfileNavbar() {
   }
 
   return (
-    <header className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${!top && 'bg-white backdrop-blur-sm shadow-lg'}`}>
+    <header
+      className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${!top && "bg-white backdrop-blur-sm shadow-lg"}`}
+    >
       <div className="max-w-6xl mx-auto px-5 sm:px-6">
         <div className="flex items-center justify-between h-16 md:h-20">
-
           <div className="flex-shrink-0 mr-4">
             {/* Logo */}
-            <Link to="/" className="block" aria-label="Notify">
-            <img src={logo} className='w-20 h-24 rounded bg-transparent'/>
+            <Link to="/" className="flex-shrink-0" aria-label="Home">
+              <img
+                src={logo}
+                className="w-12 h-12 md:w-16 md:h-16 rounded"
+                alt="Logo"
+              />
             </Link>
           </div>
 
@@ -43,14 +48,17 @@ function ProfileNavbar() {
           <nav className="flex flex-grow">
             <ul className="flex flex-grow justify-end flex-wrap items-center">
               <li>
-                <button onClick={handleLogOut} className="btn-sm text-gray200 hover:bg-gray900-87 bg-gray900 ml-3 "> {/*only redirection to homepage happening not actual sign out yet..backend functionality reqd*/}
-                  <span className='p-0.5'>Log Out</span>             
+                <button
+                  onClick={handleLogOut}
+                  className="btn-sm text-gray200 hover:bg-gray900-87 bg-gray900 ml-3 "
+                >
+                  {" "}
+                  {/*only redirection to homepage happening not actual sign out yet..backend functionality reqd*/}
+                  <span className="p-0.5">Log Out</span>
                 </button>
               </li>
             </ul>
-
           </nav>
-
         </div>
       </div>
     </header>
