cfb4eaa4849908bfe478e703bdb706c3768047ad
Harsh Rai <htc38199@gmail.com>
2024-08-29 05:40:38 +0530

    added hamburger on navbar

--- src/components/LandingPageNavbar.jsx
+++ src/components/LandingPageNavbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import logo from "/logo.png";
-import { Menu, User } from "lucide-react";
+import { Menu, X, User } from "lucide-react";
 
 function Header({ user }) {
   const [top, setTop] = useState(true);
@@ -28,7 +27,7 @@ function Header({ user }) {
           <div className="flex-shrink-0">
             <Link to="/" className="block" aria-label="Home">
               <img
-                src={logo}
+                src="/logo.png"
                 alt="Logo"
                 className="w-12 h-12 md:w-16 md:h-16 rounded"
               />
@@ -39,88 +38,124 @@ function Header({ user }) {
           <div className="md:hidden">
             <button
               onClick={toggleMenu}
-              className="text-gray-500 hover:text-gray-600"
+              className="text-gray-500 hover:text-gray-600 focus:outline-none"
             >
-              <Menu size={24} />
+              {isOpen ? <X size={24} /> : <Menu size={24} />}
             </button>
           </div>
 
           {/* Navigation */}
-          <nav className={`${isOpen ? "block" : "hidden"} md:block`}>
-            <ul className="flex flex-col md:flex-row md:items-center md:space-x-4">
-              {!user.$id ? (
-                <>
-                  <li>
-                    <Link
-                      to="/signin"
-                      className="block py-2 md:py-1 px-2 text-gray-700 hover:text-gray-900 transition duration-150 ease-in-out"
-                    >
-                      Sign in
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      to="/signup"
-                      className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-gray-900 hover:bg-gray-800 transition duration-150 ease-in-out"
-                    >
-                      <span>Sign up</span>
-                      <svg
-                        className="w-3 h-3 ml-2 -mr-1"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </Link>
-                  </li>
-                </>
-              ) : (
-                <>
-                  <li>
-                    <Link
-                      to="/allNotes"
-                      className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-gray-900 hover:bg-gray-800 transition duration-150 ease-in-out"
-                    >
-                      <span>View Notes</span>
-                      <svg
-                        className="w-3 h-3 ml-2 -mr-1"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                          clipRule="evenodd"
-                        />
-                      </svg>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="/profile" className="block">
-                      {user.avatar ? (
-                        <img
-                          src={user.url}
-                          alt="User Avatar"
-                          className="w-8 h-8 rounded-full"
-                        />
-                      ) : (
-                        <User className="w-8 h-8 text-gray-600" />
-                      )}
-                    </Link>
-                  </li>
-                </>
-              )}
+          <nav className={`hidden md:block`}>
+            <ul className="flex items-center space-x-4">
+              {renderNavItems(user)}
             </ul>
           </nav>
         </div>
       </div>
+
+      {/* Mobile menu */}
+      {isOpen && (
+        <div
+          className="fixed inset-0 z-50 bg-gray-800 bg-opacity-90 overflow-y-auto h-full w-full"
+          onClick={toggleMenu}
+        >
+          <div className="flex flex-col items-center justify-center min-h-screen px-4">
+            <button
+              onClick={toggleMenu}
+              className="absolute top-4 right-4 text-white hover:text-gray-300 focus:outline-none"
+            >
+              <X size={24} />
+            </button>
+            <ul className="flex flex-col items-center space-y-6">
+              {renderNavItems(user)}
+            </ul>
+          </div>
+        </div>
+      )}
     </header>
+  );
+}
+
+function renderNavItems(user) {
+  return !user?.$id ? (
+    <>
+      <li>
+        <Link
+          to="/signin"
+          className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-gray-900 hover:bg-gray-800 transition duration-150 ease-in-out"
+        >
+          <span>Sign in</span>
+          <svg
+            className="w-3 h-3 ml-2 -mr-1"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </Link>
+      </li>
+      <li>
+        <Link
+          to="/signup"
+          className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-gray-900 hover:bg-gray-800 transition duration-150 ease-in-out"
+        >
+          <span>Sign up</span>
+          <svg
+            className="w-3 h-3 ml-2 -mr-1"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </Link>
+      </li>
+    </>
+  ) : (
+    <>
+      <li>
+        <Link
+          to="/allNotes"
+          className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-gray-900 hover:bg-gray-800 transition duration-150 ease-in-out"
+        >
+          <span>View Notes</span>
+          <svg
+            className="w-3 h-3 ml-2 -mr-1"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </Link>
+      </li>
+      <li>
+        <Link to="/profile" className="block">
+          {user.avatar ? (
+            <img
+              src={user.url}
+              alt="User Avatar"
+              className="w-8 h-8 rounded-full"
+            />
+          ) : (
+            <User className="w-8 h-8 text-gray-600" />
+          )}
+        </Link>
+      </li>
+    </>
   );
 }
 
