0663b4d71318ba3958451a2ec1289653274b0fb8
Rahul Bansal <rahulbansal24072004@gmail.com>
2024-08-30 18:04:02 +0530

    Add form for changing college details in ProfilePage.jsx

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -22,8 +22,12 @@ function ProfilePage() {
   const [oldPassword, setoldPassword] = useState("");
   const [showChangePasswordForm, setShowChangePasswordForm] = useState(false);
   const [showChangeUsernameForm, setShowChangeUsernameForm] = useState(false);
-  const [profileImage, setProfileImage] = useState(null);
-  const [newImageUrl, setnewImageUrl] = useState("");
+  const [showChangeCollegeDetailsForm, setShowChangeCollegeDetailsForm] =
+    useState(false);
+  const [newRollNo, setNewRollNo] = useState("");
+  const [newPhoneNo, setNewPhoneNo] = useState("");
+  const [newHostel, setNewHostel] = useState("");
+  const [newRoomNo, setNewRoomNo] = useState("");
   const searchParams = useLocation();
   const [loading, setloading] = useState(false);
   const [isGoogle, setIsGoogle] = useState(false);
@@ -61,6 +65,11 @@ function ProfilePage() {
     }
   };
 
+  const collegeDetailsForm = (e) => {
+    e.preventDefault();
+    setShowChangeCollegeDetailsForm(!showChangeCollegeDetailsForm);
+  };
+
   const passwordForm = (e) => {
     e.preventDefault();
     setShowChangePasswordForm((prev) => !prev);
@@ -75,7 +84,7 @@ function ProfilePage() {
     e.preventDefault();
     try {
       setloading(true);
-      const result = await changeUserName(user.$id,newUsername);
+      const result = await changeUserName(user.$id, newUsername);
       if (result) {
         window.location.reload();
         return result;
@@ -87,7 +96,7 @@ function ProfilePage() {
     } finally {
       setloading(false);
     }
-  }
+  };
 
   const handleChangePassword = async (e) => {
     e.preventDefault();
@@ -110,33 +119,6 @@ function ProfilePage() {
       console.log(error);
       alert("Error while changing password");
       setloading(false);
-    } finally {
-      setloading(false);
-    }
-  };
-
-  const handleImageUpload = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        setProfileImage(file);
-        setnewImageUrl(reader.result);
-      };
-      reader.readAsDataURL(file);
-    }
-  };
-
-  const handleSubmitImage = async () => {
-    try {
-      setloading(true);
-      await saveProfilePicture(profileImage);
-      alert("Profile picture updated successfully");
-      setProfileImage(null);
-      setnewImageUrl("");
-    } catch (error) {
-      console.error("Error updating profile picture:", error);
-      alert("Error while updating profile picture");
     } finally {
       setloading(false);
     }
@@ -168,7 +150,7 @@ function ProfilePage() {
                     <img
                       src={user.url}
                       alt="Profile"
-                      className="rounded-full w-[30vh] h-[30vh]"
+                      className="rounded-full w-[20vh] h-[20vh]"
                     />
                   ) : (
                     <img
@@ -177,16 +159,37 @@ function ProfilePage() {
                       className="rounded-full w-[30vh] h-[30vh]"
                     />
                   )}
-                  {/* <input
-                    type="file"
-                    accept="image/*"
-                    className="hidden"
-                    onChange={handleImageUpload}
-                  /> */}
                 </label>
                 <br />
-                <h1 className="h1">{user.fullname}</h1>
-                <h2 className="h2">{user.email}</h2>
+                <div className="flex flex-col justify-center items-center gap-4 p-6 bg-white shadow-lg rounded-lg w-[24rem]">
+                  <h1 className="text-2xl font-bold text-gray-800">
+                    {user.fullname}
+                  </h1>
+                  <h2 className="text-xl text-gray-600">{user.email}</h2>
+                  <div className="flex flex-col mt-6 w-full">
+                    <p className="text-lg font-semibold text-gray-700 mb-2">
+                      College Details:
+                    </p>
+                    <div className="bg-gray-100 p-4 rounded-lg shadow-inner">
+                      <p className="text-md text-gray-800 mb-2">
+                        <span className="font-semibold">Roll No:</span>{" "}
+                        {user.roolno}
+                      </p>
+                      <p className="text-md text-gray-800 mb-2">
+                        <span className="font-semibold">Phone No:</span>{" "}
+                        {user.phoneNo}
+                      </p>
+                      <p className="text-md text-gray-800 mb-2">
+                        <span className="font-semibold">Hostel Name:</span>{" "}
+                        {user.hostel}
+                      </p>
+                      <p className="text-md text-gray-800">
+                        <span className="font-semibold">Room No:</span>{" "}
+                        {user.roomNo}
+                      </p>
+                    </div>
+                  </div>
+                </div>
               </div>
 
               {/* Form */}
@@ -279,6 +282,71 @@ function ProfilePage() {
                         <button
                           className="btn text-white bg-blue-600 hover:bg-blue-700 w-full"
                           onClick={handleChangePassword}
+                        >
+                          Submit
+                        </button>
+                      </div>
+                    </div>
+                  )}
+                  <div className="flex flex-wrap -mx-3 mt-3 mb-3">
+                    <div className="w-full px-3">
+                      <button
+                        className="btn px-0 text-white bg-gray900 hover:bg-gray-800 w-full relative flex items-center"
+                        onClick={collegeDetailsForm}
+                      >
+                        <MdOutlineDriveFileRenameOutline className="h-[30px] w-[30px]" />
+                        <span className="flex-auto pl-16 pr-8 -ml-16">
+                          Change College Details
+                        </span>
+                      </button>
+                    </div>
+                  </div>
+                  {showChangeCollegeDetailsForm && (
+                    <div className="flex flex-col w-full">
+                      <div className="w-full mb-3">
+                        <input
+                          type="text"
+                          name="newRollNo"
+                          placeholder="New Roll No"
+                          value={newRollNo}
+                          // onChange={handleChange}
+                          className="w-full p-2 border border-gray-300 rounded-lg"
+                        />
+                      </div>
+                      <div className="w-full mb-3">
+                        <input
+                          type="text"
+                          name="newPhoneNo"
+                          placeholder="New Phone No"
+                          value={newPhoneNo}
+                          // onChange={handleChange}
+                          className="w-full p-2 border border-gray-300 rounded-lg"
+                        />
+                      </div>
+                      <div className="w-full mb-3">
+                        <input
+                          type="text"
+                          name="newHostel"
+                          placeholder="New Hostel"
+                          value={newHostel}
+                          // onChange={handleChange}
+                          className="w-full p-2 border border-gray-300 rounded-lg"
+                        />
+                      </div>
+                      <div className="w-full mb-3">
+                        <input
+                          type="text"
+                          name="newRoomNo"
+                          placeholder="New Room No"
+                          value={newRoomNo}
+                          // onChange={handleChange}
+                          className="w-full p-2 border border-gray-300 rounded-lg"
+                        />
+                      </div>
+                      <div className="w-full mt-3 hover:cursor-pointer">
+                        <button
+                          className="btn text-white bg-blue-600 hover:bg-blue-700 w-full"
+                          // onClick={handleChangeCollegeDetails}
                         >
                           Submit
                         </button>
